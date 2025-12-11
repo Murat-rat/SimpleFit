d4b619455bf42332f367a0d4dc6c070c9799a4cc
ShannyRamos17 <20243ds135@utez.edu.mx>
2025-12-11 06:33:57 -0600

    Agregamos FitApiService.kt y sus funciones

---
+++ app/src/main/java/com/example/simplefit/data/remote/FitApiService.kt
@@ -0,0 +1,25 @@
+package com.example.simplefit.data.remote
+
+import com.example.simplefit.viewmodel.RunItem
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface FitApiService {
+
+    @GET("runs")
+    suspend fun getRuns(@Query("usuario") usuario: String): List<RunItem>
+
+    @POST("runs")
+    suspend fun createRun(@Body run: RunItem): RunItem
+
+    @DELETE("runs/{id}")
+    suspend fun deleteRun(@Path("id") id: Int)
+
+    @PUT("runs/{id}")
+    suspend fun updateRun(@Path("id") id: Int, @Body run: RunItem): RunItem
+}
